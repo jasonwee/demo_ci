2cd864126625d6a210d4845047b2936fa203df89
Jason Wee <peichieh@gmail.com>
2021-06-20 15:26:36 +0800

    initial

---
+++ src/main/java/ch/weetech/app/App.java
@@ -0,0 +1,13 @@
+package ch.weetech.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
